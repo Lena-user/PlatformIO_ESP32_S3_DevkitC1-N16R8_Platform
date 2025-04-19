132f75bfb28634e88abb8178a93c67b2efd68530
Lena-user <le.lena5415@gmail.com>
2025-04-19 21:42:40 +0700

    Refactor: Add PN532Reader Class

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Cấu hình giao tiếp I2C
+#define SDA_PIN 14
+#define SCL_PIN 13
+
+// Cấu hình Serial Monitor
+#define SERIAL_BAUD 115200
+
+// Cấu hình thời gian quét NFC
+#define NFC_SCAN_DELAY 2000  // 2 giây giữa mỗi lần quét
+
+#endif // CONFIG_H
